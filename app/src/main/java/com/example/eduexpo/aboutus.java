295eda46a6939e2d32c517eedc58133d7ffebbef
chaniru-puldith <a.a.d.chaniru.p@gmail.com>
2023-07-11 23:51:23 +0530

    Seventh Commit

--- app/src/main/java/com/example/eduexpo/aboutus.java
+++ app/src/main/java/com/example/eduexpo/aboutus.java
@@ -2,13 +2,25 @@ package com.example.eduexpo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class aboutus extends AppCompatActivity {
-
+    TextView about;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aboutus);
+
+        about = (TextView) findViewById(R.id.textView6);
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(aboutus.this, team.class);
+                startActivity(intent);
+            }
+        });
     }
 }
